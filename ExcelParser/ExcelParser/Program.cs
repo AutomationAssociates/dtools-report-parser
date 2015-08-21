82816d5cf9265f63e2ad745c5e6f2a8d12698ea4
Bianca Haux <bianca@aa.net.nz>
2015-08-21 14:17:35 +1200

    Added functionality to read data from .xlsx file, store it in dictionary and print it to the console.

--- ExcelParser/ExcelParser/Program.cs
+++ ExcelParser/ExcelParser/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Excel;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +14,18 @@ namespace ExcelParser
         static void Main(string[] args)
         {
 
+            Console.WriteLine("Type path of excel file:\n");
+            string path = @Console.ReadLine();
+
+            var dataReader = new ExcelDataReader(path);
+            var dictionary = dataReader.getDataEntry();
+            foreach (KeyValuePair<string, DataEntry> pair in dictionary)
+            {
+                Console.WriteLine("{0} {1}", pair.Key, pair.Value.Hours + " " + pair.Value.Price);
+            }
+            Console.ReadLine();
         }
+
     }
-}
+    
+} 
